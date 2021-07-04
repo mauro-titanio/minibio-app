d1517d9d4df8bbb7dd0aecb868c9466822f75bac
mauro-titanio <maurotitanio@gmail.com>
2021-07-04 11:12:34 +0200

    -

--- src/app/shared/services/crud/links-crud.service.ts
+++ src/app/shared/services/crud/links-crud.service.ts
@@ -16,24 +16,23 @@ export class LinksCrudService {
 
 
 //Crear un objetoo en la collección
-newLink(data: Link, userID:string) {
+newLink(data: any, userID:string) {
   return this.fireStore.collection('users').doc(userID).collection('user_links').add(data)
-  //Poniendo .add() genera id automatico
+  
 }
 //Leer toda la collección
 readAllLinks(userID: string){
-//????????
   return this.fireStore.collection('users').doc(userID).collection('user_links').get()
 }
-//Leer un post en especifico
+//Leer un link en especifico
 getLink(id:string){
   return this.fireStore.collection('users/${user.uid}/user_links').doc(id).get()
 }
-//Modificar un post
+//Modificar un link
 updateLink(userID:string, data:Link, linkID: string){
   return this.fireStore.collection('users').doc(userID).collection('user_links').doc(linkID).update(data)
 }
-//Eliminar un post
+//Eliminar un link
 deleteLink(userID:string, linkID:string){
   return this.fireStore.collection('users').doc(userID).collection('user_links').doc(linkID).delete()
 }
